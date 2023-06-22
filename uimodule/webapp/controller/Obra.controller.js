b9625a3206ec9e913960b69e2d571866b11bced0
Matilde <matilde84@gmail.com>
2023-06-22 15:05:26 -0300

    Acceso a la app Actas de suspension.

--- uimodule/webapp/controller/Obra.controller.js
+++ uimodule/webapp/controller/Obra.controller.js
@@ -421,6 +421,11 @@ sap.ui.define([
       this.navToCross("pgocontroldocumentacion", { ID })
     },
 
+    onNavigateToActasSuspension: function (oEvent) {
+      const { ID } = oEvent.getSource().getBindingContext("AppJsonModel").getObject()
+      this.navToCross("pgoactassuspension", { ID })
+    },
+
     navToCross: function (semanticObject, params) {
       sap.ushell.Container.getServiceAsync("CrossApplicationNavigation").then(oService => {
         oService.hrefForExternalAsync({
