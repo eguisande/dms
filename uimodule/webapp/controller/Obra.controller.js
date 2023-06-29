87274ee768de898b7b643bd464083add7e220f57
Matilde <matilde84@gmail.com>
2023-06-29 14:30:38 -0300

    Acceso a notas y minutas.

--- uimodule/webapp/controller/Obra.controller.js
+++ uimodule/webapp/controller/Obra.controller.js
@@ -428,6 +428,11 @@ sap.ui.define([
       this.navToCross("pgoactassuspension", { ID })
     },
 
+    onNavigateToNotasMinutas: function (oEvent) {
+      const { ID } = oEvent.getSource().getBindingContext("AppJsonModel").getObject()
+      this.navToCross("pgonotasminutas", { ID })
+    },
+
     navToCross: function (semanticObject, params) {
       sap.ushell.Container.getServiceAsync("CrossApplicationNavigation").then(oService => {
         oService.hrefForExternalAsync({
