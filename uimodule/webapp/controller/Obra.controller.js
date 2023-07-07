8dc0e941406499429caa80e6084dc5d1c1ffbd45
Matilde <matilde84@gmail.com>
2023-07-07 15:03:57 -0300

    Acceso a parte diario.

--- uimodule/webapp/controller/Obra.controller.js
+++ uimodule/webapp/controller/Obra.controller.js
@@ -433,6 +433,11 @@ sap.ui.define([
       this.navToCross("pgonotasminutas", { ID })
     },
 
+    onNavigateToParteDiario: function (oEvent) {
+      const { ID } = oEvent.getSource().getBindingContext("AppJsonModel").getObject()
+      this.navToCross("pgopartediario", { ID })
+    },
+
     navToCross: function (semanticObject, params) {
       sap.ushell.Container.getServiceAsync("CrossApplicationNavigation").then(oService => {
         oService.hrefForExternalAsync({
