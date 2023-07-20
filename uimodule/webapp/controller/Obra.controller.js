9496f7c014852a47f80d2bfee19b9c83073af203
Matilde <matilde84@gmail.com>
2023-07-20 17:28:12 -0300

    Navegacion a actas de prorroga.

--- uimodule/webapp/controller/Obra.controller.js
+++ uimodule/webapp/controller/Obra.controller.js
@@ -443,6 +443,11 @@ sap.ui.define([
       this.navToCross("pgomemoriacalculo", { ID })
     },
 
+    onNavigateToActasProrroga: function (oEvent) {
+      const { ID } = oEvent.getSource().getBindingContext("AppJsonModel").getObject()
+      this.navToCross("pgoactasprorroga", { ID })
+    },
+
     navToCross: function (semanticObject, params) {
       sap.ushell.Container.getServiceAsync("CrossApplicationNavigation").then(oService => {
         oService.hrefForExternalAsync({
