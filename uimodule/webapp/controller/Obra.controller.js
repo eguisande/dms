d93e5df0268c1706b22204e9fe0cdf809c9554b0
Matilde <matilde84@gmail.com>
2023-08-30 18:07:26 -0300

    Navagación a registros de obra.

--- uimodule/webapp/controller/Obra.controller.js
+++ uimodule/webapp/controller/Obra.controller.js
@@ -469,6 +469,11 @@ sap.ui.define([
       this.navToCross("pgoactastradicion", { ID })
     },
 
+    onNavigateToRegistrosObra: function (oEvent) {
+      const { ID } = oEvent.getSource().getBindingContext("AppJsonModel").getObject()
+      this.navToCross("pgoregistrosobra", { ID })
+    },
+
     navToCross: function (semanticObject, params) {
       sap.ushell.Container.getServiceAsync("CrossApplicationNavigation").then(oService => {
         oService.hrefForExternalAsync({
